596f31f93ac65689aded3e60bc4d0be48b01ed50
Elliot Dewhurst <elliot+github@dewhurst.me>
2016-04-25 00:28:39 +0100

    Fixed header file

--- DrunkenWalk/Grid.h
+++ DrunkenWalk/Grid.h
@@ -7,17 +7,13 @@ public:
 	Grid(int w, int h, char f, char e);
 	~Grid();
 
-	
 	int Height;
 	int Width;
 	char Fill;
 	char Empty;
 	std::vector<std::vector<char>> charVector;
 	
-	
-
 	void Print();
-	void EmptyRandomPoint();
 	int* GetRandomPoint() const;
 	void FillPoint(int x, int y);
 	void EmptyPoint(int x, int y);	
